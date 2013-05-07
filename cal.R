34fbc880210e15afedf889b4aa06c69ef0f952cf
Flip Braunisch <psysine@gmail.com>
2013-05-07 00:31:40 -0400

    modified:   cal.R 	modified:   coefs 	modified:   fayu.cc 	modified:   settings

--- cal.R
+++ cal.R
@@ -1,46 +1,56 @@
 #!/usr/bin/Rscript
-raw1 = read.table('raw', header=TRUE)
-attach(raw1)
-raw = data.frame(knew=knew, know=know, sincelast=now-pmax(lastknow,lastdunno), a=lastknow-lastdunno-(1-!!lastdunno)*(firstseen-400), b=b, nseen=nseen, nknow=nknow)
-detach(raw1)
+#knew know firstseen lastknow lastdunno nknow ndunno maxkt max2kt sumkt sumdt sumlkt sumldt now k1 k2 k3 k4 k5 k6 k7 k8 k9 k10
+raw = read.table('raw', header=TRUE)
 
-
-#add some data so that glm doesn't screw up if there isn't enough data to do the fit
-raw = rbind(raw, c(0, 0, 100, 50, 0, 2, 1))
-raw = rbind(raw, c(0, 1, 100, 50, 0, 2, 1))
-
-raw = rbind(raw, c(0, 0, 100, 50, 1, 2, 1))
-raw = rbind(raw, c(0, 1, 100, 50, 1, 2, 1))
-
-raw = rbind(raw, c(1, 0, 100, 50, 0, 2, 1))
-raw = rbind(raw, c(1, 1, 100, 50, 0, 2, 1))
-
-raw = rbind(raw, c(1, 0, 100, 50, 1, 2, 1))
-raw = rbind(raw, c(1, 1, 100, 50, 1, 2, 1))
+raw$sincelast = raw$now-pmax(raw$lastknow, raw$lastdunno)
+raw$a=raw$lastknow-ifelse(raw$lastdunno>0, raw$lastdunno, raw$firstseen-400)
 
 raw$lsl = log(1+raw$sincelast)
 raw$la = log(1+pmax(raw$a,0))
-raw$lb = log(1+raw$b)
+
+raw$nseen = raw$nknow+raw$ndunno
 raw$c = raw$nknow/raw$nseen
+raw$d = raw$nknow/(raw$ndunno+1)
+raw$lnseen = log(raw$nseen)
+raw$lmaxkt = log(1+raw$maxkt)
+raw$lmax2kt = log(1+raw$max2kt)
+raw$lsumkt = log(1+raw$sumkt)
+raw$lsumdt = log(1+raw$sumdt)
+raw$lsumlkt = log(1+raw$sumlkt)
+raw$lsumldt = log(1+raw$sumldt)
 
 formulae=c(know ~ lsl,
-           know ~ lsl + I(la*lsl) + I(la^2*lsl) + I(lb*lsl) + I(c*lsl) + la + I(la^2) + lb + c,
+           know ~ lsl + I(la*lsl)+la + I(la^2*lsl)+I(la^2) + I(lmaxkt*lsl)+lmaxkt + I(lmax2kt*lsl)+lmax2kt + I(d*lsl)+d + I(lsumkt*lsl)+lsumkt + I(lsumdt*lsl)+lsumdt + I(sumlkt*lsl)+sumlkt + I(sumldt*lsl)+sumldt  + I(lsumldt*lsl)+lsumldt + I(c*lsl)+c + I((k2+k3+k4)*lsl)+I(k2+k3+k4) + I((k5+k6+k7)*lsl)+I(k5+k6+k7) + I(lnseen*lsl)+lnseen,
            know ~ lsl + c,
-           know ~ lsl + I(lb*lsl) + I(c*lsl) + lb + c)
+           know ~ lsl + I(lmaxkt*lsl)+lmaxkt + I(lmax2kt*lsl)+lmax2kt + I(d*lsl)+d + I(lsumkt*lsl)+lsumkt + I(lsumdt*lsl)+lsumdt + I(sumlkt*lsl)+sumlkt + I(sumldt*lsl)+sumldt  + I(lsumldt*lsl)+lsumldt + I(c*lsl)+c + I((k2+k3+k4)*lsl)+I(k2+k3+k4) +I((k5+k6+k7)*lsl)+I(k5+k6+k7) + I(lnseen*lsl)+lnseen)
 
-divide = function(d) list(d$knew & !d$b,
-                          d$knew & d$b,
-                          !d$knew & !d$b,
-                          !d$knew & d$b)
-
-sections = c("knew, b=0","knew, b>0","!knew, b=0","!knew, b>0")
-div=divide(raw)
-for(j in 1:4) {
-  rawj=raw[div[[j]],]
-  m = glm(formulae[[j]], data=rawj, family='binomial')
-  cat(sections[j], "\n")
-  cat(names(m$coef), "\n", sep="   ")
-  co = m$coef
-  co[is.na(co)] = 0
-  cat(co, "\n", sep="   ")
+divide = function(d) list(d$knew & !d$maxkt,
+                          d$knew & d$maxkt,
+                          !d$knew & !d$maxkt,
+                          !d$knew & d$maxkt)
+if(1) {
+  sections = c("knew,maxkt=0","knew,maxkt>0","!knew,maxkt=0","!knew,maxkt>0")
+  div=divide(raw)
+  for(j in 1:4) {
+    rawj=raw[div[[j]],]
+    m = glm(formulae[[j]], data=rawj, family='binomial')
+    cat(sections[j], "\n")
+    cat(length(m$coef), "\n")
+    cat(gsub(' ', '', names(m$coef)), "\n", sep="   ")
+    co = m$coef
+    co[is.na(co)] = 0
+    cat(co, "\n", sep="   ")
+    }
+} else {
+  div=divide(raw)
+  raw1=raw[div[[1]],]
+  m1 = glm(formulae[[1]], data=raw1, family='binomial')
+  raw2=raw[div[[2]],]
+  m2 = glm(formulae[[2]], data=raw2, family='binomial')
+  raw3=raw[div[[3]],]
+  m3 = glm(formulae[[3]], data=raw3, family='binomial')
+  raw4=raw[div[[4]],]
+  m4 = glm(formulae[[4]], data=raw4, family='binomial')
+  m=list(m1,m2,m3,m4)
+  print(sum(sapply(m, function(x) x$deviance)))
   }
--- fayu.cc
+++ fayu.cc
@@ -6,6 +6,7 @@
 #include <vector>
 #include <algorithm>
 #include <limits>
+#include <climits>
 #include <cstdlib>
 #include <ctime>
 #include <cmath>
@@ -22,17 +23,23 @@ using namespace std;
 typedef unsigned int uint;
 typedef unsigned long long ull;
 typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<double,double> pdd;
 
 const double Inf = numeric_limits<double>::infinity();
 
+int sumi(int *a, int n) { int sum = 0; rep(i,n) sum += a[i]; return sum; }
+double sumd(double *a, int n) { double sum = 0; rep(i,n) sum += a[i]; return sum; }
+int maxi(int *a, int n) { int m = INT_MIN; rep(i,n) if(a[i] > m) m = a[i]; return m; }
+double maxd(double *a, int n) { double m = -1e300; rep(i,n) if(a[i] > m) m = a[i]; return m; }
+int mini(int *a, int n) { int m = INT_MAX; rep(i,n) if(a[i] < m) m = a[i]; return m; }
+double mind(double *a, int n) { double m = 1e300; rep(i,n) if(a[i] < m) m = a[i]; return m; }
+
 //some settings
-const unsigned batchsizemin = 8, //minimum words shown at once
-               batchsizemax = 50; //maximum words shown at once
+const unsigned batchsizemin = 4, //minimum nr of words shown at once
+               batchsizemax = 50; //maximum nr of words shown at once
 
 double target_dunno = 5; //there should be approximately how many forgotten words among the ones that are shown at once?
-
-const double ncoefs[4] = {2, 10, 3, 6};
-double coefs[4][10];
 
 string trainfilename, auxfilename;
 uint emptylines, lookahead;
@@ -54,32 +61,62 @@ struct data {
 map<string, data> db;
 map<char, string> dbs;
 
+pii get_lastkd(const vector<stamp> &v) {
+  int lastknow = 0, lastdunno = 0;
+  rep(i, v.size()) (v[i].know ? lastknow : lastdunno) = v[i].time;
+  return pii(lastknow, lastdunno);
+  }
+pii get_nkd(const vector<stamp> &v) {
+  int nknow = 0, ndunno = 0;
+  rep(i, v.size()) (v[i].know ? nknow : ndunno)++;
+  return pii(nknow, ndunno); 
+  }
+pii get_maxkt(const vector<stamp> &v) {
+  int maxkt = 0, max2kt = 0; //longest and second longest time without forgetting
+  rap(i, 1, v.size()) {
+    int kt = v[i].know*(v[i].time - v[i-1].time);
+    if(kt > maxkt) max2kt = maxkt, maxkt = kt;
+    else if(kt > max2kt) max2kt = kt;
+    }
+  return pii(maxkt, max2kt);
+  }
+pii get_sumkdt(const vector<stamp> &v) {
+  int sumk = 0, sumd = 0;
+  rap(i, 1, v.size()) (v[i].know ? sumk : sumd) += v[i].time - v[i-1].time;
+  return pii(sumk, sumd);
+  }
+pdd get_sumlkdt(const vector<stamp> &v) {
+  double sumk = 0, sumd = 0;
+  rap(i, 1, v.size()) (v[i].know ? sumk : sumd) += log(1 + v[i].time - v[i-1].time);
+  return pdd(sumk, sumd);
+  }
+
 struct params {
-  int firstseen, knew, lastknow, lastdunno, b, nseen, nknow;
+  int firstseen, knew, lastknow, lastdunno, nknow, ndunno, maxkt, max2kt, sumkt, sumdt, k[10];
+  double sumlkt, sumldt;
   time_t now;
   };
 
 void getparams(const vector<stamp> &v, time_t now, params *p) {
-  int firstseen;
   if(v.empty()) {
-    firstseen = 0;
+    cerr << "getparams called with empty v\n";
+    exit(1);
     return;
     }
-  firstseen = v[0].time;
-  int prev = 0, lastdunno = 0, lastknow = 0,
-      b = 0, // longest time without forgetting, but is decreased once we forget twice in a row
-      nseen = 0, nknow = 0;
-  iter(it, v) {
-    if(prev > 0 && it->know && it->time - prev > b)
-      b = it->time - prev;
-    else if(prev > 0 && !it->know && it->time - prev < b)
-      b = (b + it->time - prev)/2;
-    (it->know ? lastknow : lastdunno) = it->time;
-    nseen++;
-    if(it->know) nknow++;
-    prev = it->time;
-    }
-  *p = {firstseen, v.back().know, lastknow, lastdunno, b, nseen, nknow, now};
+  p->firstseen = v[0].time;
+  p->knew = v[v.size()-1].know;
+  pii lastkd = get_lastkd(v);
+  p->lastknow = lastkd.F, p->lastdunno = lastkd.S;
+  pii nkd = get_nkd(v);
+  p->nknow = nkd.F, p->ndunno = nkd.S;
+  pii maxkt = get_maxkt(v);
+  p->maxkt = maxkt.F, p->max2kt = maxkt.S;
+  pii sumkdt = get_sumkdt(v);
+  p->sumkt = sumkdt.F, p->sumdt = sumkdt.S;
+  rep(i, 10) p->k[i] = int(v.size()) > i ? v[v.size()-1-i].know*1 : 1;
+  pdd sumlkdt = get_sumlkdt(v);
+  p->sumlkt = sumlkdt.F, p->sumldt = sumlkdt.S;
+  p->now = now;
   }
 
 double ilogit(double x) {
@@ -87,29 +124,57 @@ double ilogit(double x) {
   return t/(1+t);
   }
 
+const int npredstot = 30;
+int npreds[4], predinds[4][npredstot];
+const char *prednames[npredstot] = {
+	"(Intercept)",		"lsl", 			"I(la*lsl)", 		"la",
+	"I(la^2*lsl)",		"I(la^2)", 		"I(lmaxkt*lsl)", 	"lmaxkt",
+	"I(lmax2kt*lsl)", 	"lmax2kt", 		"I(d*lsl)", 		"d",
+	"I(lsumkt*lsl)", 	"lsumkt",		"I(lsumdt*lsl)",	"lsumdt",
+	"I(sumlkt*lsl)", 	"sumlkt", 		"I(sumldt*lsl)",	"sumldt",
+	"I(lsumldt*lsl)", 	"lsumldt", 		"I(c*lsl)",		"c",
+	"I((k2+k3+k4)*lsl)", 	"I(k2+k3+k4)", 		"I((k5+k6+k7)*lsl)", 	"I(k5+k6+k7)",
+	"I(lnseen*lsl)", 	"lnseen"};
+double coefs[4][npredstot];
+
 double rate(const vector<stamp> v) {
+  if(v.empty()) return 0;
   params p;
   getparams(v, time(0)+lookahead, &p);
-  
   int knew=p.knew,
       sincelast=p.now-max(p.lastknow,p.lastdunno),
-      a=p.lastknow-p.lastdunno-(1-!!p.lastdunno)*(p.firstseen-400),
-      b=p.b,
-      nseen = p.nseen,
-      nknow = p.nknow;
+      a=p.lastknow - (p.lastdunno ? p.lastdunno : p.firstseen-400),
+      maxkt = p.maxkt,
+      nseen = p.nknow + p.ndunno,
+      nknow = p.nknow,
+      ndunno = p.ndunno;
   double lsl = log(1+sincelast),
          la = log(1+a),
-         lb = log(1+b),
-         c = double(nknow)/nseen;
-  double params[4][10] = {{1,lsl,0,0,0,0,0,0,0,0},
-                          {1,lsl,la*lsl,la*la*lsl,lb*lsl,c*lsl,la,la*la,lb,c},
-                          {1,lsl,c,0,0,0,0,0,0,0},
-                          {1,lsl,lb*lsl,c*lsl,lb,c,0,0,0,0}};
-  if(!p.firstseen) return 0;
-  int ind = 2*!knew + !!b;
+         lmaxkt = log(1+maxkt),
+         lmax2kt = log(1+p.max2kt),
+         c = double(nknow)/nseen,
+	 d = nknow/(1.+ndunno),
+         lsumkt = log(1+p.sumkt),
+         lsumdt = log(1+p.sumdt),
+         sumlkt = p.sumlkt,
+         sumldt = p.sumldt,
+         lsumldt = log(1+sumldt),
+         lnseen = log(nseen),
+         k234 = sumi(p.k+1, 3),
+         k567 = sumi(p.k+4, 3);
+  double preds[] = {
+	1,			lsl,			la*lsl,			la,
+	la*la*lsl,		la*la,			lmaxkt*lsl,		lmaxkt,
+	lmax2kt*lsl,		lmax2kt,		d*lsl,			d,
+	lsumkt*lsl,		lsumkt,			lsumdt*lsl,		lsumdt,
+	sumlkt*lsl,		sumlkt,			sumldt*lsl,		sumldt,
+	lsumldt*lsl,		lsumldt,		c*lsl,			c,
+	k234*lsl,		k234,			k567*lsl,		k567,
+	lnseen*lsl,		lnseen};
+  int ind = 2*!knew + !!maxkt;
   double sum = 0;
-  rep(i, ncoefs[ind])
-    sum += coefs[ind][i]*params[ind][i];
+  rep(i, npreds[ind])
+    sum += coefs[ind][i]*preds[predinds[ind][i]];
   return ilogit(sum);
   }
 
@@ -205,7 +270,7 @@ bool finish(string filename) {
   char ch;
   while(getline2(in, s))
     if(!s.empty()) {
-      if(s[s.size()-1] == ':')
+      if(s[s.size()-1] == ':' || s[0] == '#')
         continue;
       if(s == "old" || (s.size() == 5 && s.substr(0, 4) == "new " && dbs.count(ch = s[4]))) {
         if(hasoldornew) {
@@ -376,7 +441,7 @@ void savesettings() {
 
 void printraw() {
   ofstream out("raw");
-  out << "knew know firstseen lastknow lastdunno b nseen nknow now\n";
+  out << "knew know firstseen lastknow lastdunno nknow ndunno maxkt max2kt sumkt sumdt sumlkt sumldt now k1 k2 k3 k4 k5 k6 k7 k8 k9 k10\n";
   iter(it, db) {
     params p;
     vector<stamp> v = it->S.stamps;
@@ -386,7 +451,12 @@ void printraw() {
     v.pop_back();
     while(v.size() > 0) {
       getparams(v, now, &p);
-      out<<p.knew<<' '<<know<<' '<<p.firstseen<<' '<<p.lastknow<<' '<<p.lastdunno<<' '<<p.b<<' '<<p.nseen<<' '<<p.nknow<<' '<<p.now<<'\n';
+      out<<p.knew<<' '<<know<<' '<<p.firstseen<<' '<<p.lastknow<<' '
+         <<p.lastdunno<<' '<<p.nknow<<' '<<p.ndunno<<' '<<p.maxkt<<' '
+         <<p.max2kt<<' '<<p.sumkt<<' '<<p.sumdt<<' '<<p.sumlkt<<' '
+         <<p.sumldt<<' '<<p.now<<' ';
+      rep(i,10) out<<p.k[i]<<' ';
+      out<<'\n';
       now = v.back().time;
       know = v.back().know;
       v.pop_back();
@@ -394,14 +464,26 @@ void printraw() {
     }
   }
 
+
+int getpredind(string s) {
+  rep(i, npredstot) if(s == prednames[i]) return i;
+  return -1;
+  }
+
 void loadcoefs() {
   ifstream in("coefs");
+  string tmp;
   rep(i, 4) {
-    in.ignore(100000, '\n');
-    in.ignore(100000, '\n');
-    rep(j, ncoefs[i])
+    in >> tmp;
+    in >> npreds[i];
+    if(npreds[i] > npredstot)
+      cerr << "too many predictors\n", exit(1);
+    rep(j, npreds[i]) {
+      in >> tmp, predinds[i][j] = getpredind(tmp);
+      if(predinds[i][j] < 0) cerr<<"unknown predictor: "<<tmp<<endl, exit(1);
+      }
+    rep(j, npreds[i])
       in >> coefs[i][j];
-    in.ignore(100000, '\n');
     }
   }
 
@@ -459,7 +541,6 @@ int main(int argc, char **argv) {
 #define starts(a, b) ((b) == string(a).substr(0, b.size()))
   if(starts("statistics", command))
     {stat(); return 0;}
-  //  {analyze(); return 0;}
   else if(starts("continue", command)) {
     if(argc >= 3)
       target_dunno = atof(argv[2]);
@@ -468,6 +549,8 @@ int main(int argc, char **argv) {
     if(finish(trainfilename))
       start();
     }
+  else if(starts("finish", command))
+    finish(trainfilename);
   else if(starts("auxiliary", command))
     finish(auxfilename);
   else if(starts("calibrate", command))
